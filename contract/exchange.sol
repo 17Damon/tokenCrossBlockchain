05ca41c4bc7fdbcb2996dbd2c47550a61695c764
miss61008596 <61008596@qq.com>
2017-12-02 21:20:14 +0800

    edit

--- contract/exchange.sol
+++ contract/exchange.sol
@@ -30,13 +30,18 @@ contract Exchange {
         //destroy token done
         toAddr = _toAddr;
         voucher = makeVoucher(_toAddr,_amount);
+        vouchers[msg.sender].voucher = voucher;
+        vouchers[msg.sender].used = false;
+        vouchers[msg.sender].isFrom = false;
         EventDestroyToken(_toAddr,voucher,_amount);
     }
 
     function getToken(uint256 _amount) external returns (bool success){
-        require(vouchers[msg.sender].isFrom == true);
-        require(vouchers[msg.sender].used == false);
-        require(vouchers[msg.sender].voucher == makeVoucher(msg.sender,_amount));
+        require(
+            vouchers[msg.sender].isFrom == true &&
+            vouchers[msg.sender].used == false &&
+            vouchers[msg.sender].voucher == makeVoucher(msg.sender,_amount)
+            );
         vouchers[msg.sender].used = true;
         //issue token done
         success = true;
@@ -63,9 +68,6 @@ contract Exchange {
         for (uint i = 0; i < 32; i++) tempBytes[k++] = bytes32(_toAddr)[i];
         for (i = 0; i < 32; i++) tempBytes[k++] = bytes32(_amount)[i];
         voucher = keccak256(tempString);
-        vouchers[msg.sender].voucher = voucher;
-        //vouchers[msg.sender].used = false;
-        vouchers[msg.sender].isFrom = false;
     }
 
 }
